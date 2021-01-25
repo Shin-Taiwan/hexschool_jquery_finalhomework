a2615c22c5b5c7e528e07c5ad13905fc8afce516
Shin <windyqoo@yahoo.com.tw>
2021-01-25 17:07:29 +0900

    fix

--- js/script.js
+++ js/script.js
@@ -13,4 +13,35 @@ var mySwiper = new Swiper('.swiper-container', {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
-})
+});
+
+$(document).ready(function () {
+    $('.pulldown').click(function (e) { 
+        e.preventDefault();
+        $(this).toggleClass('active');
+        $(this).parent().find('ul').slideToggle();
+        $(this).parent().siblings().find('a').removeClass('active');
+        $(this).parent().siblings().find('ul').slideUp();
+        
+    });
+
+    $('.toTop a').click(function (e) { 
+        e.preventDefault();
+        $('html,body').animate({
+            scrollTop:0
+        },1000)
+    });
+
+    $(window).scroll(function(){
+        if($(this).scrollTop()>300){
+            $('.toTop').fadeIn();
+        }else{
+            $('.toTop').fadeOut();
+        }
+    });
+    lightbox.option({
+        'resizeDuration': 2000,
+        'wrapAround': true,
+        'imageFadeDuration':400,
+    })
+});
